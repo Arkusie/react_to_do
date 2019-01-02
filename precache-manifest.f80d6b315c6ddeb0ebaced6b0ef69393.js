8fad35fc5443d84dea8527380c0e12d2d3138f46
Arek <majkhrhr@gmail.com>
2019-01-02 19:33:45 +0100

    Updates

---
+++ precache-manifest.f80d6b315c6ddeb0ebaced6b0ef69393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "792edf34d4b5e12c0c3c",
+    "url": "/react_to_do/static/js/runtime~main.792edf34.js"
+  },
+  {
+    "revision": "b653407c1f6bf8878c8f",
+    "url": "/react_to_do/static/js/main.b653407c.chunk.js"
+  },
+  {
+    "revision": "5db1da23b91c42607fae",
+    "url": "/react_to_do/static/js/1.5db1da23.chunk.js"
+  },
+  {
+    "revision": "b653407c1f6bf8878c8f",
+    "url": "/react_to_do/static/css/main.44eb011b.chunk.css"
+  },
+  {
+    "revision": "0f59841d590e6d869129efb65033bd02",
+    "url": "/react_to_do/index.html"
+  }
+];
