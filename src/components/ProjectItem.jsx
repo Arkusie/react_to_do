d24c322165b56cc55d3c5b45a98e47fcce287c4d
Arek <majkhrhr@gmail.com>
2019-01-04 00:20:38 +0100

    class into function(ProjectItem)

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
 
-class ProjectItem extends Component {
-  deleteProject(id) {
-    // it sould be executed in the main component(App),
-    //so it needs to be passed +2 up(into Projects and then into App)
-    console.log("delete test");
-    this.props.onDelete(id);
-  }
-
-  render() {
-    return (
-      <li>
-        <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>
-          {" "}
-          X
-        </a>
-        {this.props.project.title} - {this.props.project.category}{" "}
-      </li>
-    );
-  }
+function ProjectItem(props) {
+  return (
+    <li>
+      <a href="#" onClick={props.onDelete}>
+        {" "}
+        X
+      </a>
+      {props.project.title} - {props.project.category}{" "}
+    </li>
+  );
 }
 
 export default ProjectItem;
